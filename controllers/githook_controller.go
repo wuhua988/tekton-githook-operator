c3c1ca970a003511fb3ead29062dc26c2fd7f445
张海东 <haidongdev@gmail.com>
2020-02-26 16:49:28 +0800

    update reconcile function

--- controllers/githook_controller.go
+++ controllers/githook_controller.go
@@ -19,28 +19,96 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	toolsv1alpha1 "github.com/zhd173/githook/api/v1alpha1"
 )
 
+const (
+	controllerAgentName = "githook-controller"
+	finalizerName       = controllerAgentName
+)
+
 // GitHookReconciler reconciles a GitHook object
 type GitHookReconciler struct {
 	client.Client
-	Log logr.Logger
+	Log          logr.Logger
+	Scheme       *runtime.Scheme
+	WebhookImage string
+}
+
+func (r *GitHookReconciler) requestLogger(req ctrl.Request) logr.Logger {
+	return r.Log.WithName(req.NamespacedName.String())
+}
+
+func ignoreNotFound(err error) error {
+	if apierrs.IsNotFound(err) {
+		return nil
+	}
+	return err
 }
 
 // +kubebuilder:rbac:groups=tools.github.com/zhd173,resources=githooks,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=tools.github.com/zhd173,resources=githooks/status,verbs=get;update;patch
 
+// Reconcile ...
 func (r *GitHookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("githook", req.NamespacedName)
+	log := r.requestLogger(req)
 
-	// your logic here
+	log.Info("Reconciling" + req.NamespacedName.String())
 
-	return ctrl.Result{}, nil
+	// Fetch the GitHook instance
+	sourceOrg := &toolsv1alpha1.GitHook{}
+	err := r.Get(context.Background(), req.NamespacedName, sourceOrg)
+	if err != nil {
+		// requeue the request
+		return ctrl.Result{}, ignoreNotFound(err)
+	}
+
+	source := sourceOrg.DeepCopyObject()
+
+	var reconcileErr error
+	if sourceOrg.ObjectMeta.DeletionTimestamp == nil {
+		// 新建、更新
+		reconcileErr = r.reconcile(source.(*toolsv1alpha1.GitHook))
+	} else {
+		// 删除：通过 DeletionTimestamp != nil 判定是否删除，调用 finalize 方法删除依赖资源
+		if r.hasFinalizer(source.(*toolsv1alpha1.GitHook).Finalizers) {
+			reconcileErr = r.finalize(source.(*toolsv1alpha1.GitHook))
+		}
+	}
+
+	if err := r.Update(context.Background(), source); err != nil {
+		log.Error(err, "Failed to update")
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, reconcileErr
+
+}
+
+// 新建、更新逻辑
+func (r *GitHookReconciler) reconcile(*toolsv1alpha1.GitHook) error {
+	// 1. 获取 Knative Service，不存在则创建，否则更新
+	// 2. 使用上一步给出的 Knative Service URL 注册 git webhook，并保存返回的 ID
+	// 3. 更新 GitHook 资源状态
+	return nil
+}
+
+// 删除逻辑
+func (r *GitHookReconciler) finalize(*toolsv1alpha1.GitHook) error {
+	return nil
+}
+
+func (r *GitHookReconciler) hasFinalizer(finalizers []string) bool {
+	for _, finalizerStr := range finalizers {
+		if finalizerStr == finalizerName {
+			return true
+		}
+	}
+	return false
 }
 
 func (r *GitHookReconciler) SetupWithManager(mgr ctrl.Manager) error {
--- go.mod
+++ go.mod
@@ -3,12 +3,31 @@ module github.com/zhd173/githook
 go 1.13
 
 require (
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-logr/logr v0.1.0
-	github.com/onsi/ginkgo v1.10.1
-	github.com/onsi/gomega v1.7.0
-	github.com/tektoncd/pipeline v0.10.1
-	k8s.io/api v0.17.0
-	k8s.io/apimachinery v0.17.1
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
+	github.com/gogits/go-gogs-client v0.0.0-20190616193657-5a05380e4bc2
+	github.com/google/go-cmp v0.3.0 // indirect
+	github.com/google/go-containerregistry v0.0.0-20190617215043-876b8855d23c // indirect
+	github.com/google/go-github/v26 v26.0.9
+	github.com/google/uuid v1.1.1
+	github.com/knative/build v0.6.0 // indirect
+	github.com/knative/pkg v0.0.0-20190618011146-4ce60b7cdefc
+	github.com/knative/serving v0.6.1
+	github.com/mattbaird/jsonpatch v0.0.0-20171005235357-81af80346b1a // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	github.com/prometheus/common v0.0.0-20181020173914-7e9e6cabbd39 // indirect
+	github.com/prometheus/procfs v0.0.0-20181005140218-185b4288413d // indirect
+	github.com/tektoncd/pipeline v0.4.0
+	github.com/xanzy/go-gitlab v0.18.0
+	golang.org/x/net v0.0.0-20190501004415-9ce7a6920f09
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	gopkg.in/go-playground/webhooks.v5 v5.11.0
+	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
+	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
-	sigs.k8s.io/controller-runtime v0.2.2
+	knative.dev/pkg v0.0.0-20191110170412-a805b647f3f2 // indirect
+	sigs.k8s.io/controller-runtime v0.2.0-alpha.1
+	sigs.k8s.io/controller-tools v0.2.0-beta.2 // indirect
 )
