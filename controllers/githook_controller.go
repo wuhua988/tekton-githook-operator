16d0cc61e5cb765a084b1f56122c1854b4e539d0
张海东 <haidongdev@gmail.com>
2020-02-26 17:34:06 +0800

    add reconcileWebhookService function to create or update Knative Service

--- controllers/githook_controller.go
+++ controllers/githook_controller.go
@@ -17,18 +17,27 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/go-logr/logr"
+	servingv1beta1 "github.com/knative/serving/pkg/apis/serving/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	servinv1alpha1 "github.com/knative/serving/pkg/apis/serving/v1alpha1"
 	toolsv1alpha1 "github.com/zhd173/githook/api/v1alpha1"
 )
 
 const (
 	controllerAgentName = "githook-controller"
+	runKsvcAs           = "pipeline-runner" // see tektonrole.yaml
 	finalizerName       = controllerAgentName
 )
 
@@ -44,6 +53,10 @@ func (r *GitHookReconciler) requestLogger(req ctrl.Request) logr.Logger {
 	return r.Log.WithName(req.NamespacedName.String())
 }
 
+func (r *GitHookReconciler) sourceLogger(source *toolsv1alpha1.GitHook) logr.Logger {
+	return r.Log.WithName(fmt.Sprintf("%s/%s", source.Namespace, source.Name))
+}
+
 func ignoreNotFound(err error) error {
 	if apierrs.IsNotFound(err) {
 		return nil
@@ -53,6 +66,11 @@ func ignoreNotFound(err error) error {
 
 // +kubebuilder:rbac:groups=tools.github.com/zhd173,resources=githooks,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=tools.github.com/zhd173,resources=githooks/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=tools.pongzt.com,resources=githooks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=tools.pongzt.com,resources=githooks/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=serving.knative.dev,resources=services,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
+// +kubebuilder:rbac:groups=eventing.knative.dev,resources=channels,verbs=get;list;watch
 
 // Reconcile ...
 func (r *GitHookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
@@ -81,6 +99,7 @@ func (r *GitHookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		}
 	}
 
+	// 更新 GitHook 资源状态
 	if err := r.Update(context.Background(), source); err != nil {
 		log.Error(err, "Failed to update")
 		return ctrl.Result{}, err
@@ -90,11 +109,161 @@ func (r *GitHookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 }
 
 // 新建、更新逻辑
-func (r *GitHookReconciler) reconcile(*toolsv1alpha1.GitHook) error {
-	// 1. 获取 Knative Service，不存在则创建，否则更新
-	// 2. 使用上一步给出的 Knative Service URL 注册 git webhook，并保存返回的 ID
-	// 3. 更新 GitHook 资源状态
+func (r *GitHookReconciler) reconcile(source *toolsv1alpha1.GitHook) error {
+	log := r.sourceLogger(source)
+	ksvc, err := r.reconcileWebhookService(source)
+	if err != nil {
+		return err
+	}
+
+	// 使用 Knative Service URL 注册 git webhook，并保存返回的 ID
 	return nil
+}
+
+// 调和 Knative Service，不存在则创建，否则更新
+func (r *GitHookReconciler) reconcileWebhookService(source *toolsv1alpha1.GitHook) (*servinv1alpha1.Service, error) {
+	log := r.sourceLogger(source)
+
+	desiredKsvc, err := r.generateKnativeServiceObject(source, r.WebhookImage)
+	if err != nil {
+		return nil, err
+	}
+
+	ksvc, err := r.getOwnedKnativeService(source)
+	if err != nil {
+		if !apierrs.IsNotFound(err) {
+			return nil, fmt.Errorf("Failed to verify if knative service is created for the gogssource: " + err.Error())
+		}
+
+		// no webhook service found, create new
+		log.Info("webhook service not exist. create new one.")
+		if err = r.Create(context.TODO(), desiredKsvc); err != nil {
+			return nil, err
+		}
+		ksvc = desiredKsvc
+		log.Info("webhook service created successfully", "name", ksvc.Name)
+	}
+
+	// should update
+	if ksvc != desiredKsvc {
+
+		templateUpdated := !apiequality.Semantic.DeepEqual(
+			desiredKsvc.Spec.ConfigurationSpec.Template.Spec.PodSpec,
+			ksvc.Spec.ConfigurationSpec.Template.Spec.PodSpec)
+
+		if templateUpdated == true {
+			log.Info("webhook service template update")
+			desiredKsvc.Spec.ConfigurationSpec.Template.Spec.PodSpec.DeepCopyInto(&ksvc.Spec.ConfigurationSpec.Template.Spec.PodSpec)
+
+			if err = r.Update(context.TODO(), ksvc); err != nil {
+				return nil, err
+			}
+			log.Info("webhook service template update successfully")
+		}
+	}
+
+	log.Info("ensure webhook service is ready", "ksvc name", ksvc.Name)
+	ksvc, err = r.waitForKnativeServiceReady(source)
+	if err != nil {
+		return nil, err
+	}
+	log.Info("webhook service is ready", "ksvc name", ksvc.Name)
+
+	return ksvc, err
+}
+
+// 生成期望 Knative Service 对象
+func (r *GitHookReconciler) generateKnativeServiceObject(source *toolsv1alpha1.GitHook, receiveAdapterImage string) (*servinv1alpha1.Service, error) {
+	labels := map[string]string{
+		"receive-adapter": source.Name,
+	}
+	env := []corev1.EnvVar{
+		{
+			Name: "SECRET_TOKEN",
+			ValueFrom: &corev1.EnvVarSource{
+				SecretKeyRef: source.Spec.SecretToken.SecretKeyRef,
+			},
+		},
+	}
+
+	runSpecJSON, err := json.Marshal(source.Spec.RunSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	containerArgs := []string{
+		fmt.Sprintf("--gitprovider=%s", source.Spec.GitProvider),
+		fmt.Sprintf("--namespace=%s", source.Namespace),
+		fmt.Sprintf("--name=%s", source.Name),
+		fmt.Sprintf("--runSpecJSON=%s", string(runSpecJSON)),
+	}
+
+	ksvc := &servinv1alpha1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: fmt.Sprintf("%s-webhook-", source.Name),
+			Namespace:    source.Namespace,
+			Labels:       labels,
+		},
+		Spec: servinv1alpha1.ServiceSpec{
+			ConfigurationSpec: servinv1alpha1.ConfigurationSpec{
+				Template: &servinv1alpha1.RevisionTemplateSpec{
+					Spec: servinv1alpha1.RevisionSpec{
+						RevisionSpec: servingv1beta1.RevisionSpec{
+							PodSpec: servingv1beta1.PodSpec{
+								ServiceAccountName: runKsvcAs,
+								Containers: []corev1.Container{corev1.Container{
+									Image: receiveAdapterImage,
+									Env:   env,
+									Args:  containerArgs,
+								}},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(source, ksvc, r.Scheme); err != nil {
+		return nil, err
+	}
+	return ksvc, nil
+}
+
+var (
+	jobOwnerKey = ".metadata.controller"
+)
+
+// 获取旧的 Knative Service
+func (r *GitHookReconciler) getOwnedKnativeService(source *toolsv1alpha1.GitHook) (*servinv1alpha1.Service, error) {
+	ctx := context.Background()
+
+	list := &servinv1alpha1.ServiceList{}
+	if err := r.List(ctx, list, client.InNamespace(source.Namespace), client.MatchingField(jobOwnerKey, source.Name)); err != nil {
+		return nil, fmt.Errorf("unable to list knative service %s", err)
+	}
+
+	if len(list.Items) <= 0 {
+		return nil, apierrs.NewNotFound(servinv1alpha1.Resource("ksvc"), "")
+	}
+
+	return &list.Items[0], nil
+}
+
+func (r *GitHookReconciler) waitForKnativeServiceReady(source *toolsv1alpha1.GitHook) (*servinv1alpha1.Service, error) {
+	for attempts := 0; attempts < 4; attempts++ {
+		ksvc, err := r.getOwnedKnativeService(source)
+		if err != nil {
+			return nil, err
+		}
+		routeCondition := ksvc.Status.GetCondition(servinv1alpha1.ServiceConditionRoutesReady)
+		receiveAdapterAddr := ksvc.Status.Address
+		if routeCondition != nil && routeCondition.Status == corev1.ConditionTrue && receiveAdapterAddr != nil {
+			return ksvc, nil
+		}
+		time.Sleep(2000 * time.Millisecond)
+	}
+	return nil, fmt.Errorf("Failed to get service to be in ready state")
 }
 
 // 删除逻辑
